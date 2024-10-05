a69495e4f95f8c8b16c1fd0ed2c700a71cfe4dfb
veronica <220711948@students.uajy.ac.id>
2024-10-05 15:38:42 +0700

    Update Tugas_Modul3_A_VERO_11948.py

--- Tugas_Modul3_A_VERO_11948.py
+++ Tugas_Modul3_A_VERO_11948.py
@@ -73,7 +73,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'"D:\UAJY\UAJY_sem_5\ML\ML4\Supervised Learning (Praktek)\Tugas3_A_11948"'
+    model_directory = r'SVR_IPK_model.pkl'
     model_path = os.path.join(model_directory, r'SVR_IPK_model.pkl')
 
     if os.path.exists(model_path):
